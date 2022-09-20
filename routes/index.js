8683f0f7d649436cc458db7bd78998847ec435da
dhengghuring <wyvrendragon@gmail.com>
2022-09-20 21:45:26 +0700

    Membuat halaman 404 & Menambahkan efek transisi untuk container

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const { redirectLink } = require("../controllers/shortened.controller");
+const express = require('express');
+const { redirectLink } = require('../controllers/shortened.controller');
 const router = express.Router();
 
-const shortenedRouter = require("./shortened.route");
+const shortenedRouter = require('./shortened.route');
 
 /* GET home page. */
-router.get("/", (req, res, next) => res.render("index"));
-router.get("/:id", redirectLink);
-router.use("/shorten", shortenedRouter);
+router.get('/', (req, res, next) => res.render('index'));
+router.get('/:id', redirectLink);
+router.use('/shorten', shortenedRouter);
 
 module.exports = router;
