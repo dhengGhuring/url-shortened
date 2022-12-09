fe298341223d155288483e617a2fa876f53bb81f
Ibrahim <ihim512@gmail.com>
2022-12-09 20:22:08 +0700

    add atlas config

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ module.exports = {
   rootPath: path.resolve(__dirname, ".."),
   serviceName: process.env.SERVICE_NAME,
   jwtKey: process.env.SECRET,
-  urlDb: process.env.MONGO_URL,
+  urlDb: process.env.MONGO_ATLAS,
 };
